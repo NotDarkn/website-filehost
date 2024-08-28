d166031e0b2a62957c3cf495c78b31567e09fd68
Darkn <privateservice000@gmail.com>
2024-08-27 22:07:27 -0400

    feat: add all files from old to here
    - update icons
    - update embeds
    - update api
    - update text

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -4,11 +4,11 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
-        <Image src="/images/fabulous-rip-2.png" alt="404" width={912} height={912} priority />
+        <Image src="/images/fabulous-rip-2.png" alt="404" width={684} height={684} priority />
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
-          Oops, that&apos;s a <span className="underline decoration-red-500 decoration-wavy">four-oh-four</span>.
+          <span className="underline decoration-red-500 decoration-wavy">! How did you get here?</span>.
         </div>
         <div className="mb-4 overflow-hidden break-all rounded border border-gray-400/20 bg-gray-50 p-2 font-mono text-xs dark:bg-gray-800">
           {errorMsg}
@@ -18,16 +18,16 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
           <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
             F12
           </kbd>{' '}
-          and open devtools for more details, and report this issue to{' '}
+          and open devtools for more details, or seek help in my{' '}
           <a
             className="text-blue-600 hover:text-blue-700 hover:underline"
-            href="https://github.com/lyc8503/onedrive-cf-index-ng/issues"
+            href="https://discord.com/invite/ER8saRhYpg"
             target="_blank"
             rel="noopener noreferrer"
           >
-            onedrive-cf-index-ng issues
+            Discord DMs
           </a>
-          .
+          !
         </div>
       </div>
     </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="OneDrive Vercel Index" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />  
+          <meta name="description" content="OneDrive CF Index" />
+          <meta property="og:title" content="Darkn's File Host" />
+          <meta property="og:description" content="Another File Host" />
+          <meta property="og:url" content="https://dl.darkn.bio" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://darkn.bio/img/download.webp" />
+          <meta property="og:image:alt" content="Files" />
+          <meta property="og:image:width" content="512" />
+          <meta property="og:image:height" content="512" />
+          <meta property="og:site_name" content="Darkn" />
+          <link rel="icon" href="/icons/download.webp" />  
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
