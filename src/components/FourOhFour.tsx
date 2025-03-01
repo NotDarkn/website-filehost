cc1732c350cb4e80cc8be0178c33c76e99aa93a0
Darkn <privateservice000@gmail.com>
2025-02-28 23:22:01 -0500

    chore: add notice

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -14,18 +14,16 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
           {errorMsg}
         </div>
         <div className="text-sm">
-          Press{' '}
-          <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
-            F12
+          Heya, if you're seeeing this,
           </kbd>{' '}
-          and open devtools for more details, or seek help in my{' '}
+          my file host is officially dead. I'm sorry. {' '}
           <a
             className="text-blue-600 hover:text-blue-700 hover:underline"
-            href="https://discord.com/invite/ER8saRhYpg"
+            href="https://darkn.bio/notice"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Discord DMs
+            <b>Read more here.</b>
           </a>
           !
         </div>
